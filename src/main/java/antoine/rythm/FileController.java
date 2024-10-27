99685c9171f9ebd2d5492d0770b7409c9520ec41
Antoine Hazebrouck <antoine.hazebrouck.etu@univ-lille.fr>
2024-10-27 15:24:01 +0100

    form submit .osz, retrieve beatmap + sound

--- src/main/java/antoine/rythm/FileController.java
+++ src/main/java/antoine/rythm/FileController.java
@@ -1,33 +1,46 @@
 package antoine.rythm;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 public class FileController {
-	private final ZipFile zip;
+	private ZipFile zip;
 
-	public FileController(ResourceLoader resourceLoader) throws ZipException, IOException {
-		this.zip = new ZipFile(
-				resourceLoader.getResource("classpath:osu/2070917 Tan Bionica - Ciudad Magica.osz").getFile());
+	@PostMapping(path = "/setup-game")
+	public RedirectView setupGame(
+			@RequestParam("note-spacing") int noteSpacing,
+			@RequestParam("osu-archive") MultipartFile osuArchive)
+			throws ZipException, IOException, IllegalStateException, URISyntaxException {
+		File temp = File.createTempFile("temp", ".osz");
+		osuArchive.transferTo(temp);
+
+		this.zip = new ZipFile(temp);
+
+		return new RedirectView("/index");
 	}
 
 	@GetMapping(path = "/audio")
-	public ResponseEntity<InputStreamResource> audio() throws IOException {
+	public ResponseEntity<ByteArrayResource> audio() throws IOException {
 		InputStream stream = zip.getInputStream(zip.getEntry("audio.ogg"));
 		return ResponseEntity.ok()
 				.contentType(MediaType.asMediaType(MimeType.valueOf("audio/ogg")))
-				.body(new InputStreamResource(stream));
+				.body(new ByteArrayResource(stream.readAllBytes()));
 	}
 
 	@GetMapping(path = "/beatmap")
@@ -38,4 +51,5 @@ public class FileController {
 				.contentType(MediaType.TEXT_PLAIN)
 				.body(new String(stream.readAllBytes()));
 	}
+
 }
