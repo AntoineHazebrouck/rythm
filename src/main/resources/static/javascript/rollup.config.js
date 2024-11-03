1cfff78585272fc7f8fc4228d71fb3d56e3ab273
Antoine Hazebrouck <antoine.haz@gmail.com>
2024-11-03 22:05:59 +0100

    out dir for typescript transpiling

--- src/main/resources/static/javascript/rollup.config.js
+++ src/main/resources/static/javascript/rollup.config.js
@@ -1,7 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default {
-  input: './src/main.js',
+  input: './dist/main.js',
   output: {
     file: 'bundle.js'
   },
---
+++ src/main/resources/static/javascript/src/main.ts
@@ -0,0 +1,16 @@
+
+import { setupCounter } from './counter'
+
+document.querySelector<HTMLDivElement>('#note-rating')!.innerHTML = `
+
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
