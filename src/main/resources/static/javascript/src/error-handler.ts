4b12647276c37e5b67934e655c2a9b6b0e42012b
Antoine Hazebrouck <antoine.haz@gmail.com>
2024-11-09 00:27:12 +0100

    refactor error displaying

--- src/main/resources/static/javascript/src/error-handler.ts
+++ src/main/resources/static/javascript/src/error-handler.ts
@@ -1,7 +1,5 @@
 const errorTag = document.querySelector("#error-message")!;
 
-errorTag.innerHTML = 'Error';
-
 export function displayError(error: Error): void {
 	errorTag.innerHTML = error.message;
 }
--- src/main/resources/static/javascript/src/main.ts
+++ src/main/resources/static/javascript/src/main.ts
@@ -3,19 +3,28 @@ import { HitsHandler } from './hits-handler';
 import { addEventListeners } from './inputs-handler';
 import { getParameter } from './parameters-handler';
 import { DisplayHandler } from './display-handler';
+import { displayError } from './error-handler';
 
-const osuMap = await fetch(
-	getParameter('beatmap-url').orElseThrow(
-		new Error('Could not read note-spacing property')
-	)
-).then((data) => data.text());
-const hitsHandler = new HitsHandler(
-	new BeatmapDecoder().decodeFromString(osuMap)
-);
-const displayHandler = new DisplayHandler(
-	hitsHandler,
-	document.querySelector('canvas')!
-);
+try {
+	const osuMap = await fetch(
+		getParameter('beatmap-url').orElseThrow(
+			new Error('Could not read note-spacing property')
+		)
+	).then((response) => {
+		if (response.ok) return response.text();
+		else throw new Error(`HTTP ${response.status} for ${response.url}`);
+	});
 
-addEventListeners(hitsHandler);
-displayHandler.startDisplaying();
+	const hitsHandler = new HitsHandler(
+		new BeatmapDecoder().decodeFromString(osuMap)
+	);
+	const displayHandler = new DisplayHandler(
+		hitsHandler,
+		document.querySelector('canvas')!
+	);
+
+	addEventListeners(hitsHandler);
+	displayHandler.startDisplaying();
+} catch (error) {
+	displayError(error);
+}
