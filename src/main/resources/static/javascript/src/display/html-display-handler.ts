79ca0e1649864b5864e0fa5e1bd34d901a2ac17a
Antoine Hazebrouck <antoine.haz@gmail.com>
2024-11-11 19:19:32 +0100

    note displaying good

--- src/main/resources/static/javascript/src/display/html-display-handler.ts
+++ src/main/resources/static/javascript/src/display/html-display-handler.ts
@@ -1,13 +1,25 @@
 import { HitResult } from 'osu-classes';
 
 export class HtmlDisplayHandler {
-	private readonly noteRating: HTMLElement;
-
-	public constructor(noteRating: HTMLElement) {
-		this.noteRating = noteRating;
-	}
+	public constructor(private readonly noteRating: HTMLElement) {}
 
 	public displayRating(rating: HitResult): void {
-		this.noteRating.innerHTML = HitResult[rating];
+		const resultToAlertMapping = {
+			[HitResult.Perfect]: 'alert-success',
+			[HitResult.Great]: 'alert-success',
+			[HitResult.Good]: 'alert-success',
+			[HitResult.Ok]: 'alert-warning',
+			[HitResult.Meh]: 'alert-warning',
+			[HitResult.Miss]: 'alert-danger',
+		};
+		this.noteRating.innerHTML = `
+			<div class="alert ${
+				resultToAlertMapping[rating]
+					? resultToAlertMapping[rating]
+					: 'alert-secondary'
+			} col-4 m-3 mx-auto" role="alert">
+				<h1 class="text-center">${rating}</h1>
+			</div>
+		`;
 	}
 }
