23832438dbc733a43c4e3d999529c455e085dad1
Antoine Hazebrouck <antoine.hazebrouck.etu@univ-lille.fr>
2025-01-06 21:28:17 +0100

    error message for error in settings

--- src/main/java/antoine/rythm/controllers/ControllerAdvisor.java
+++ src/main/java/antoine/rythm/controllers/ControllerAdvisor.java
@@ -1,16 +1,9 @@
 package antoine.rythm.controllers;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +14,5 @@ class ControllerAdvisor {
 	public OAuth2User getCurrentUser(@AuthenticationPrincipal OAuth2User principal) {
 
 		return principal;
-	}
-
-	// https://stackoverflow.com/questions/73638868/springboot-controller-send-400-on-validation-error
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<List<String>> handleConstraintViolationException(MethodArgumentNotValidException cve) {
-		List<String> errorMessages = cve.getFieldErrors()
-				.stream()
-				.map(error -> error.getField() + " : " + error.getDefaultMessage())
-				.toList();
-
-		return new ResponseEntity<>(errorMessages, HttpStatus.BAD_REQUEST);
 	}
 }
--- src/main/java/antoine/rythm/controllers/UserSettingsController.java
+++ src/main/java/antoine/rythm/controllers/UserSettingsController.java
@@ -1,13 +1,18 @@
 package antoine.rythm.controllers;
 
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
 import antoine.rythm.controllers.dto.UserSettingsDto;
@@ -38,6 +43,19 @@ class UserSettingsController {
 		UserEntity user = userService.asUserEntity(principal);
 
 		userService.save(userSettings.toEntity(user));
+
+		return new RedirectView("/user/settings");
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public RedirectView handleConstraintViolationException(RedirectAttributes attributes, MethodArgumentNotValidException cve) {
+		List<String> errorMessages = cve.getFieldErrors()
+				.stream()
+				.map(error -> error.getField() + " : " + error.getDefaultMessage())
+				.toList();
+
+		
+		attributes.addAttribute("error", errorMessages);
 
 		return new RedirectView("/user/settings");
 	}
